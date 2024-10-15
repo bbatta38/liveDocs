64b0d79a372314ecd765a3aa569b54e73e655e70
bbatta38 <bbatta38@gmail.com>
2024-10-15 21:17:15 +1300

    added types and header component

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,13 @@
 import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
 import React from "react";
 
 const Documents = () => {
     return (
         <div>
+            <Header>
+                <p>Test</p>
+            </Header>
             <Editor />
         </div>
     );
